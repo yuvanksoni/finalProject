7221343d3873e51a41e49867aeabc202ef6b3b9d
Yuvank Soni <yuvanksoni@gmail.com>
2020-11-30 18:36:16 +0530

    Add files via upload

---
+++ portal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rick is sup4r cool</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/bootstrap.min.css">
+  <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <div class="container">
+  </br><img width="300" src="assets/portal.jpg"><h3>Portal Login Page</h3></br>
+    <form name="input" action="" method="post">
+      <label for="username">Username:</label><input type="text" class="form-control" value="" id="username" name="username" />
+      <label for="password">Password:</label><input type="password" class="form-control" value="" id="password" name="password" />
+
+      
+    </br><input type="submit" value="Login" class="btn btn-success" name="sub"/>
+    </form>
+  </div>
+
+</body>
+</html>
